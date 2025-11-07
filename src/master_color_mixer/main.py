364ba54279495c0cf2086cfea1a02b6416eec32f
Amanda Crotty <fullstackmandurlorian@gmail.com>
2025-11-07 11:32:55 -0500

    update command

--- src/master_color_mixer/main.py
+++ src/master_color_mixer/main.py
@@ -4,7 +4,7 @@ import uvicorn
 
 def main():
     print("Hello MasterColorMixer (starting API...)")
-    uvicorn.run("mastercolormixer.app:app", host="127.0.0.1", port=8000, reload=False)
+    uvicorn.run("src.master_color_mixer.app:app", host="127.0.0.1", port=8000, reload=False)
 
 if __name__ == "__main__":
     main()

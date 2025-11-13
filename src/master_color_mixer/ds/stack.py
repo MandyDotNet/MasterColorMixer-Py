4c4199db470d8fac05ace83af610353509a2aee9
Amanda Crotty <fullstackmandurlorian@gmail.com>
2025-11-13 10:52:07 -0500

    updated Stack to meet assignment requirement

--- src/master_color_mixer/ds/stack.py
+++ src/master_color_mixer/ds/stack.py
@@ -1,10 +1,10 @@
 # with push, pop, peek, is_empty (wrap python list)
 
-class Stack:
+from typing import Generic, Iterable, List, TypeVar
 
-    def __init__(self, items = None):
+T = TypeVar("T")
 
-        if items is None:
-            self._data = []
-        else:
-            self._data = list(items)
+class Stack(Generic[T]):
+    # implement a custom stack class: LIFO stack by Python list
+    def __init__(self, initial: Iterable[T] | None = None) -> None:
+        self._data: List[T] = list(initial) if initial is not None else []

ebe479e293df17685a5ac2c95320a47ab687766d
Amanda Crotty <fullstackmandurlorian@gmail.com>
2025-11-13 13:48:32 -0500

    comments for guidance

--- tests/tests_ds.py
+++ tests/tests_ds.py
@@ -1,3 +1,52 @@
 import pytest
 
 from src.mastercolormixer.ds import DynamicArray, Stack, RingBufferQueue, HashSet
+
+#-- Test DynamicArray --
+
+#append
+
+#pop
+
+#__getitem__
+
+#__setitem__
+
+#__len__
+
+
+#-- Test Stack --
+
+#push
+
+#pop
+
+#peek
+
+#is_empty
+
+#__len_
+
+
+#-- Test RingBufferQueue --
+
+#enqueue
+
+#dequeue
+
+#peek
+
+#is_empty
+
+#__len__
+
+
+#-- Test HashSet --
+
+#add
+
+#remove
+
+#contains
+
+#__len__

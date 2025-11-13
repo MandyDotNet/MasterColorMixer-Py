956576defea66974e87c1a1d3a459b14ce0ed89c
Amanda Crotty <fullstackmandurlorian@gmail.com>
2025-11-13 13:09:16 -0500

    updated hashset with all required methods

--- src/master_color_mixer/ds/hashset.py
+++ src/master_color_mixer/ds/hashset.py
@@ -1,11 +1,23 @@
 # with add, remove, contains, __len__ (compose Python set)
 
-class HashSet:
+from typing import Generic, Iterable, Set,TypeVar
 
-    def __init__(self, items = None):
+T = TypeVar("T")
 
-        #class set
-        if items is None:
-            self._set = set()
-        else:
-            self._set = set(items) #dedicated class structure to call methods
+class HashSet(Generic[T]): # I like to implement with Type saftey, to prevent bugs. Mirrors how we use HashSeT<T> in C#
+
+    # can be any iterable type like list or set / stored as set(T) or an empty set()
+    def __init__(self, initial: Iterable[T] | None = None) -> None:
+        self._set: Set[T] = set(initial) if initial is not None else set()
+
+    def add(self, item: T) -> None:
+        self._set.add(item)
+
+    def remove(self, item: T) -> None:
+        self._set.remove(item)
+
+    def contains(self, item: T) -> bool:
+        return item in self._set
+
+    def __len__(self) -> int:
+        return len(self._set)

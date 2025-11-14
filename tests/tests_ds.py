be3d0688df9c3d3fe79367e119bbebe22b9d7e39
Amanda Crotty <fullstackmandurlorian@gmail.com>
2025-11-14 12:12:29 -0500

    completed queue tests

--- tests/tests_ds.py
+++ tests/tests_ds.py
@@ -26,7 +26,7 @@ def test_dynamic_array_append_getitem_setitem_pop_and_len():
     assert len(theDA) == 0 #uses the __len__ method to assert that after 10 pops, all items are gone
 #--> edge cases covered: overwrite at an index, clearing the DynamicArray object
 
-def test_dynamic_array_pop_empty_error():
+def test_dynamic_array_pop_empty_IndexError():
     da = DynamicArray[int]()
     with pytest.raises(IndexError): #assert what error should occur
         da.pop()
@@ -55,7 +55,7 @@ def test_stack_push_pop_peek_len_isEmpty():
     assert len(stak) == 0
 #--> edge case covered: clear Stack
 
-def test_stack_pop_and_peek_empty_raises():
+def test_stack_pop_and_peek_empty_IndexError():
     stak = Stack[int]()
     with pytest.raises(IndexError):
         stak.pop()
@@ -65,7 +65,34 @@ def test_stack_pop_and_peek_empty_raises():
 
 
 #-- Test RingBufferQueue --
+def test_queue_enqueue_dequeue_isEmpty_len_():
+    que = RingBufferQueue[int]()  # default capacity is 8
 
+    # enqueue items over 8 
+    values = list(range(20))
+    for v in values:
+        que.enqueue(v)  # when v = 8, resize makes capacity 16; when v = 16 -> capacity is 32
+
+    assert len(que) == 20   # verify the number of stored elements
+    assert not que.is_empty() # redundant assert
+    # I did not write a capacity helper, so we cannot test that the internal array capacity is 32.
+
+    # dequeue first 10, should be in FIFO order
+    first_batch = [que.dequeue() for _ in range(10)]
+    assert first_batch == values[10] # verify up to index 10 that values match
+    assert len(que) == 10   #check that we drained half the queue
+
+    # drain the rest
+    remaining = [que.dequeue() for _ in range(len(que))]
+    assert remaining == values[10]
+    assert que.is_empty()
+
+def test_queue_empty_dequeue_and_peek_IndexError():
+    q = RingBufferQueue[int]()
+    with pytest.raises(IndexError):
+        q.dequeue()
+    with pytest.raises(IndexError):
+        q.peek()
 
 #-- Test HashSet --
 

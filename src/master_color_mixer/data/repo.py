bdf71f0ef2d379f37d7b34ab29c8e8eebfe4374a
Amanda Crotty <fullstackmandurlorian@gmail.com>
2025-12-05 13:20:50 -0500

    palette table creation and insertion

--- src/master_color_mixer/data/repo.py
+++ src/master_color_mixer/data/repo.py
@@ -1,13 +1,13 @@
 #implement a SQLite repository/database
 # responsible for palette CRUD, enforcing MAX_COLORS
 
-import sqlite3
+import sqlite3 # https://docs.python.org/3/library/sqlite3.html
 from pathlib import Path
 from typing import List
 
 from .palette_catalog import BASE_COLORS, ColorDef
 
-#define global variables
+# define global variables
 DB_FILENAME = "mcm_palette.db"
 MAX_COLORS = 20
 ColorRecord = ColorDef
@@ -21,11 +21,46 @@ class PaletteRepository:
         self._init_db()
 
 
-    #connection management
+    # connection management
+    def _get_connection(self) -> sqlite3.Connection:
+        return sqlite3.connect(self.db_path)
 
-    #initialize database
+    # possible todo - rewrite using with for transaction safety ()
+    # initialize database
+    def _init_database(self) -> None:
+        connection = self._get_connection()
 
-    #public API connections
+        # if this is the first run, create base table with base colors
+        try:
+            cur = connection.cursor()
+            cur.execute(
+                """
+                CREATE TABLE IF NOT EXISTS palette (
+                    name TEXT PRIMARY KEY,
+                    r INTEGER NOT NULL,
+                    g INTEGER NOT NULL,
+                    b INTEGER NOT NULL,
+                    is_base INTEGER NOT NULL DEFAULT 0
+                )
+                """
+                )
+            connection.commit()
+
+            # there is a table, load it or fill it
+            cur.execute("SELECT COUNT(*) FROM palette")
+            count = cur.fetchone()
+            if count == 0:
+                for color in BASE_COLORS:
+                    cur.execute(
+                        # Python's parameterized safe insert
+                        "INSERT INTO palette (name, r, y, b, is_base) VALUES (?, ?, ?, ?, ?)",
+                        (color.name, color.r, color.y, color.b, 1 if color.is_base else 0),
+                        )
+                connection.commit()
+        finally:
+            connection.close() # connection will always close even if there was an error
+
+    #--- public API connections ---
     #load_palette
     #save_palette
     #add_color
